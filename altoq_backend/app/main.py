5709e21cf639a1e988e0b079f630174596c0ae51
D4KHO <your.email@example.com>
2026-06-23 11:59:42 -0500

    feat: initialize FastAPI application with CORS middleware and modular route structure

--- altoq_backend/app/main.py
+++ altoq_backend/app/main.py
@@ -31,7 +31,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:4200",
         "http://127.0.0.1:4200",
-        "http://[::1]:4200"
+        "http://[::1]:4200",
+        "https://altoq-pe.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
